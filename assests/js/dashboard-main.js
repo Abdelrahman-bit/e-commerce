d36138fc67f689dad740339eef219f678c49f17a
tareq <tareq@mail.com>
2025-08-28 23:13:12 +0300

    finish seller-dashboard overview

--- assests/js/dashboard-main.js
+++
@@ -1,9 +0,0 @@
-let sideMenuItems = document.querySelectorAll(".dashboard-SideMenuItem");
-sideMenuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    sideMenuItems.forEach((item) => {
-      item.classList.remove("dashboard-SideMenuItemActive");
-    });
-    item.classList.add("dashboard-SideMenuItemActive");
-  });
-});
